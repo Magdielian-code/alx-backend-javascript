9cb424f9d53cd3deedcd7713bb38886795294cc5
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-02 15:41:02 +0100

    task: finished task 0; to modify functions to use const and let instead of var.

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+export function taskFirst() {
+    const task = "I preger const when i can";
+    return task;
+  }
+  
+  export function getLast() {
+    return "is okay";
+  }
+  
+  export function taskNext() {
+      let combination = "But sometimes let";
+      combination += getLast();
+  
+      return combination;
+  }
+  
+  
