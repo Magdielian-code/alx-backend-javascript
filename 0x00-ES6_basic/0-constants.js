b6d72096b55f545937370b108861149b197c36c5
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-02 15:46:41 +0100

    issue: fixed code format error.

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,15 @@
 export function taskFirst() {
-    const task = "I preger const when i can";
-    return task;
-  }
-  
-  export function getLast() {
-    return "is okay";
-  }
-  
-  export function taskNext() {
-      let combination = "But sometimes let";
-      combination += getLast();
-  
-      return combination;
-  }
-  
-  
+  const task = "I preger const when i can";
+  return task;
+}
+
+export function getLast() {
+  return "is okay";
+}
+
+export function taskNext() {
+  let combination = "But sometimes let";
+  combination += getLast();
+
+  return combination;
+}
