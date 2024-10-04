95839cccb37817dbdabec0c8b75730d1cdbcff87
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-04 02:24:04 +0100

    task: Finally, write a function named iterateThroughObject. The function’s parameter reportWithIterator is the return value from createIteratorObject.

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,17 @@
+// export default function iterateThroughObject(reportWithIterator) {
+//   let output = "";
+
+//   for (const [index, item] of Object.entries(reportWithIterator)) {
+//     output += `${item}`;
+
+//     if (parseInt(index) !== reportWithIterator.length - 1) {
+//       output += " | ";
+//     }
+//   }
+
+//   return output;
+// }
+
+export default function iterateThroughObject(reportWithIterator) {
+    return [...reportWithIterator].join(' | ');
+}
