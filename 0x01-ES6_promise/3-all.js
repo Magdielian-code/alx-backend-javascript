c69e5558368344cdf15936aea8a7c5c58953dba8
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-05 11:19:49 +0100

    task: Using the following prototype
    function signUpUser(firstName, lastName) {
    }
    
    That returns a resolved promise with this object:

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
-import {uploadPhoto, createUser} from './utils.js';
+import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then((values) => {
-            console.log(values[0].body, values[1].body);
-        })
-        .catch(() => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(values[0].body, values[1].body);
+    })
+    .catch(() => console.log("Signup system offline"));
 }
+                                                                                                                                                                                                                                                                          
