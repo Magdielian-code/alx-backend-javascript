c5627424fb04dcbf571290961b792b2c7bd02152
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-05 02:50:32 +0100

    task: In this file, import uploadPhoto and createUser from utils.js
    Knowing that the functions in utils.js return promises, use the prototype below to collectively resolve all promises and log body firstName lastName to the console.

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import {uploadPhoto, createUser} from './utils.js';
+
+export default function handleProfileSignup() {
+    return Promise.all([uploadPhoto(), createUser()])
+        .then((values) => {
+            console.log(values[0].body, values[1].body);
+        })
+        .catch(() => console.log('Signup system offline'));
+}
