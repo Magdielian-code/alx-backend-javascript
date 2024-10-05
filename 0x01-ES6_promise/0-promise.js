3dde470df3a6c338a371a12144f331d6d5d29b00
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-05 01:00:42 +0100

    task: set up environment for project, Return a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,2 +1,58 @@
-const promise = doSomething();
-const promise2 = promise.then(successCallback, failureCallback);
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {});
+}
+
+
+
+
+
+
+// const promise = doSomething();
+// const promise2 = promise.then(successCallback, failureCallback);
+
+// let p = new Promise((resolve, reject) => {
+//     let a = 1 + 1;
+//     if (a == 2) {
+//         resolve('Success');
+//     } else {
+//         reject('Failed');
+//     }
+// });
+
+// p.then((message) => {
+//     console.log('This is in the then ' + message);
+// }).catch((message) => {
+//     console.log('This is in the catch ' + message);
+// })
+
+// const recordVideoOne = new Promise((resolve, reject) => {
+//     resolve('Video 1 Recorded');
+// })
+
+// const recordVideoTwo = new Promise((resolve, reject) => {
+//     resolve('Video 2 Recorded');
+// })
+
+// const recordVideoThree = new Promise((resolve, reject) => {
+//     resolve('Video 3 Recorded');
+// })
+
+// Promise.all([
+//     recordVideoOne,
+//     recordVideoTwo,
+//     recordVideoThree
+// ]).then((messages) => {
+//     console.log(messages);
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+// Promise.race([
+//     recordVideoOne,
+//     recordVideoTwo,
+//     recordVideoThree
+// ]).then((messages) => {
+//     console.log(messages);
+// }).catch((error) => {
+//     console.log(error)
+// })
