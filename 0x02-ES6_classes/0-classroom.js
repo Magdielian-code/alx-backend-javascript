4b9d272b8be541cdd5842915c57bff4ea4d803fc
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-09 13:24:52 +0100

    Task: Implement a class named ClassRoom:
        Prototype: export default class ClassRoom
        It should accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,28 +1,36 @@
-class Product{
-    salesTax = 8.25;
-
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
+export default class ClassRoom {
+    constructor(maxStudentSize) {
+        this.maxStudentSize = maxStudentSize;
     }
-
-    displayProduct(){
-        console.log(`Product: $${this.name}`);
-        console.log(`Price: $${this.price.tofixed(2)}`);
-    }
-
-    calculateTotal(salesTax){
-        return this.price + (this.price * (salesTax / 100));
-    }
-
 }
 
-const product1 = new Product("Shirt", 19.99);
-const product2 = new Product("Pants", 29.99);
 
 
-product1.displayProduct();
-product2.displayProduct();
+// class Product{
+//     salesTax = 8.25;
 
-const total1 = product1.calculateTotal(salesTax);
-console.log(`Total price with Tax: $${total1.tofixed(2)}`);
+//     constructor(name, price){
+//         this.name = name;
+//         this.price = price;
+//     }
+
+//     displayProduct(){
+//         console.log(`Product: $${this.name}`);
+//         console.log(`Price: $${this.price.tofixed(2)}`);
+//     }
+
+//     calculateTotal(salesTax){
+//         return this.price + (this.price * (salesTax / 100));
+//     }
+
+// }
+
+// const product1 = new Product("Shirt", 19.99);
+// const product2 = new Product("Pants", 29.99);
+
+
+// product1.displayProduct();
+// product2.displayProduct();
+
+// const total1 = product1.calculateTotal(salesTax);
+// console.log(`Total price with Tax: $${total1.tofixed(2)}`);
