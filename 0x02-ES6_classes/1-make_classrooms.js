ea5f0ec26eb7b91dbd8678c1fed4065e64e8a68b
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-09 16:19:28 +0100

    task: import initializeRooms from './1-make_classrooms.js';
    console.log(initializeRooms());

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,8 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from "./0-classroom.js";
 
-export function initializeRooms() {
+export default function initializeRooms() {
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
-  const classes = [class1, class2, class3];
-  return (classes);
+  return [class1, class2, class3];
 }
