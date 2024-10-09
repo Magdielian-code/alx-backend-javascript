3794705c97fa97e9bc23b6380b7b5ee8c31db6fc
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-09 13:26:44 +0100

    Added new line as requsted

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,36 +1,6 @@
 export default class ClassRoom {
-    constructor(maxStudentSize) {
-        this.maxStudentSize = maxStudentSize;
-    }
+  constructor(maxStudentSize) {
+    this.maxStudentSize = maxStudentSize;
+  }
 }
 
-
-
-// class Product{
-//     salesTax = 8.25;
-
-//     constructor(name, price){
-//         this.name = name;
-//         this.price = price;
-//     }
-
-//     displayProduct(){
-//         console.log(`Product: $${this.name}`);
-//         console.log(`Price: $${this.price.tofixed(2)}`);
-//     }
-
-//     calculateTotal(salesTax){
-//         return this.price + (this.price * (salesTax / 100));
-//     }
-
-// }
-
-// const product1 = new Product("Shirt", 19.99);
-// const product2 = new Product("Pants", 29.99);
-
-
-// product1.displayProduct();
-// product2.displayProduct();
-
-// const total1 = product1.calculateTotal(salesTax);
-// console.log(`Total price with Tax: $${total1.tofixed(2)}`);
