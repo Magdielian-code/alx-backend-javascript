cff52fd9ac7badda73c59f1321a2ca9fbc9f4a98
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-10 13:22:43 +0100

    task: Import Building from 5-building.js.
    Implement a class named SkyHighBuilding that extends from Building:
    
        Constructor attributes:
            sqft (Number) (must be assigned to the parent class Building)
            floors (Number)
        Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
        Implement a getter for each attribute.
        Override the method named evacuationWarningMessage and return the following string Evacuate slowly the NUMBER_OF_FLOORS floors.

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,14 @@
+import Building from "./6-building.js";
+
+export default class SkyHIghBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
+    }
+    get floors() {
+        return this._floors;
+    }
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
+}
