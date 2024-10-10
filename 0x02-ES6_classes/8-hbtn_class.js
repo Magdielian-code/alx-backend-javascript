264f86af13f5e4e4b05cb57dc520c58573ebd35d
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-10 14:00:18 +0100

    task: [Symbol.toPrimitive](hint) {         if (hint === 'number') {             return this._size;         } else if (hint === 'string') {             return this._location;         }         return null;     }

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,26 +1,27 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        this._size = size;
-        this._location = location;
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+  get size() {
+    return this._size;
+  }
+  set size(size) {
+    this._size = size;
+  }
+  get location() {
+    return this._location;
+  }
+  set location(location) {
+    this._location = location;
+  }
+  [Symbol.toPrimitive](hint) {
+    if (hint === "number") {
+      return this._size;
+    } else if (hint === "string") {
+      return this._location;
+    } else {
+      return this._size;
     }
-    get size() {
-        return this._size;
-    }
-    set size(size) {
-        this._size = size;
-    }
-    get location() {
-        return this._location;
-    }
-    set location(location) {
-        this._location = location;
-    }
-    [Symbol.toPrimitive](hint) {
-        if (hint === 'number') {
-            return this._size;
-        } else if (hint === 'string') {
-            return this._location;
-        }
-        return null;
-    }
+  }
 }
