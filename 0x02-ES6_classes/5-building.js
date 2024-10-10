2c3db5c77d56285da3c88d77a4e883e01f5b4282
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-10 12:18:23 +0100

    task: Implement a class named Building:
        Constructor attributes:
            sqft (Number)
        Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
        Implement a getter for each attribute.
        Consider this class as an abstract class. And make sure that any class that extends from it should implement a method named evacuationWarningMessage.
            If a class that extends from it does not have a evacuationWarningMessage method, throw an error with the message Class extending Building must override evacuationWarningMessage

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error("Class BUilding cannot be instantiated directly ");
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== "function") {
+        throw new Error(
+          "Class extending Building must override evacuationWarningMessage"
+        );
+      }
     }
     if (typeof sqft !== "number") {
       throw TypeError("sqft must be a number");
@@ -10,11 +14,5 @@ export default class Building {
   }
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMethod() {
-    throw new Error(
-      "Class extending Building must override evacuationWarningMessage"
-    );
   }
 }
