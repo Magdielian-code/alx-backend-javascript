aba76ea6cc47f9c9310b90bc190f8320eac88403
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-11 15:50:45 +0100

    task: Create a function getListStudentIds that returns an array of ids from a list of object.
    This function is taking one argument which is an array of objects - and this array is the same format as getListStudents from the previous task.
    
    If the argument is not an array, the function is returning an empty array.
    
    You must use the map function on the array.

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,37 +1,7 @@
-// JavaScript Examples
-// 1. Shallow Copy Examples
-const originalArray = [1, [2, 3], { a: 4 }];
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
 
-
-// Using spread operator
-const spreadCopy = [...originalArray];
-console.log(spreadCopy)
-
-// Using Array.from()
-const fromCopy = Array.from(originalArray);
-console.log(fromCopy)
-
-// Using slice()
-const sliceCopy = originalArray.slice();
-console.log(sliceCopy)
-
-
-
-// 2. Deep Copy Implementation in JavaScript
-function deepCopy(obj) {
-    if (obj === null || typeof obj !== 'object') return obj;
-    
-    const copy = Array.isArray(obj) ? [] : {};
-    
-    for (const key in obj) {
-        copy[key] = deepCopy(obj[key]);
-    }
-    
-    return copy;
+  return array.map((item) => item.id);
 }
-
-// Using deep copy
-const deepCopied = deepCopy(originalArray);
-originalArray[1].push(5);
-console.log(originalArray);     // [1, [2, 3, 4, 5], { a: 4 }]
-console.log(deepCopied);        // 2. Deep Copy Implementation in JavaScript
