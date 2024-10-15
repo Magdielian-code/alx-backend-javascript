5a1b445fc17fcdeb54445b84c1234a5850d59f10
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-15 13:27:28 +0100

    task: Create a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade
    It should accept a list of students (from getListStudents), a city (String), and newGrades (Array of “grade” objects) as parameters.
    
    newGrades is an array of objects with this format:
    
      {
        studentId: 31,
        grade: 78,
      }
    
    If a student doesn’t have grade in newGrades, the final grade should be N/A.
    
    You must use filter and map combined.

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,87 +1,99 @@
-// export default function updateStudentGradeByCity(array, city, newGrade) {
-//   return array.filter((item) => item.location === city).map((student) => {
-//     const
-//   });
-// }
-const map1 = ["1", "2", "3"].map((x) => parseInt(x, 10));
-console.log(map1);
+export default function updateStudentGradeByCity(array, city, newGrade) {
+  return array
+    .filter((item) => item.location === city)
+    .map((student) => {
+      newGrade.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          student.grade = studentGrade.grade;
+        }
+        if (!student.grade) {
+          student.grade = "N/A";
+        }
+        return student;
+      });
+      return student;
+    });
+}
 
-const numbers = [1, 2, 3, 4, 5];
-const filteredNumbers = numbers.map((numbers, index) => {
-  if (index < 3) {
-    return numbers;
-  }
-});
-console.log(filteredNumbers);
+// const map1 = ["1", "2", "3"].map((x) => parseInt(x, 10));
+// console.log(map1);
 
-const cities = ["lonDon", "ManCHESTer", "BiRmiNGHAM", "liVERpoOL"];
-const selected = cities.filter((city, index) => {
-  if (index < 3) {
-    return city.toUpperCase();
-  }
-});
-console.log(selected);
+// const numbers = [1, 2, 3, 4, 5];
+// const filteredNumbers = numbers.map((numbers, index) => {
+//   if (index < 3) {
+//     return numbers;
+//   }
+// });
+// console.log(filteredNumbers);
 
-const cart = [5, 40, 23];
-let total = 0;
-const withTax = cart.map((cost) => {
-  total += cost;
-  return cost * 1.05;
-});
+// const cities = ["lonDon", "ManCHESTer", "BiRmiNGHAM", "liVERpoOL"];
+// const selected = cities.filter((city, index) => {
+//   if (index < 3) {
+//     return city.toUpperCase();
+//   }
+// });
+// console.log(selected);
 
-console.log(withTax);
-console.log(total);
+// const cart = [5, 40, 23];
+// let total = 0;
+// const withTax = cart.map((cost) => {
+//   total += cost;
+//   return cost * 1.05;
+// });
 
-const products = [
-  { name: "apples", price: 2 },
-  { name: "oranges", price: 3 },
-  { name: "bananas", price: 1 },
-];
+// console.log(withTax);
+// console.log(total);
 
-const updatedProducts = products.map((product) => {
-  return { ...product, price: `${product.price + 100}` };
-});
+// const products = [
+//   { name: "apples", price: 2 },
+//   { name: "oranges", price: 3 },
+//   { name: "bananas", price: 1 },
+// ];
 
-console.log(updatedProducts);
+// const updatedProducts = products.map((product) => {
+//   return { ...product, price: `${product.price + 100}` };
+// });
 
-const Numbers = [1, 4, 6, -7, -2, 5, 7, -9, 4];
-const averaged = Numbers.filter((numbers) => numbers > 0).map(
-  (numbers, index, arr) => {
-    const prev = arr[index - 1];
-    const next = arr[index + 1];
+// console.log(updatedProducts);
 
-    let count = 1;
-    let total = numbers;
-    if (prev !== undefined) {
-      count++;
-      total += prev;
-    }
-    if (next !== undefined) {
-      count++;
-      total += next;
-    }
-    const average = total / count;
-    return Math.round((average * 100) / 100);
-  }
-);
-console.log(averaged);
+// const Numbers = [1, 4, 6, -7, -2, 5, 7, -9, 4];
+// const averaged = Numbers.filter((numbers) => numbers > 0).map(
+//   (numbers, index, arr) => {
+//     const prev = arr[index - 1];
+//     const next = arr[index + 1];
 
-const arrayLike = {
-  length: 4,
-  0: "foo",
-  1: "bar",
-  2: "baz",
-  3: 2,
-  4: 3,
-  5: 4,
-};
+//     let count = 1;
+//     let total = numbers;
+//     if (prev !== undefined) {
+//       count++;
+//       total += prev;
+//     }
+//     if (next !== undefined) {
+//       count++;
+//       total += next;
+//     }
+//     const average = total / count;
+//     return Math.round((average * 100) / 100);
+//   }
+// );
+// console.log(averaged);
 
-const arrayLikeArray = Array.from(arrayLike);
-const arrayLikeArray2 = Object.entries(arrayLike).map(([key, value]) => {
-  if (typeof value === "string") {
-    return [key, "changed"];
-  }
-  return [key, value];
-});
-console.log(arrayLikeArray2);
-console.log(Object.fromEntries(arrayLikeArray2));
+// const arrayLike = {
+//   length: 4,
+//   0: "foo",
+//   1: "bar",
+//   2: "baz",
+//   3: 2,
+//   4: 3,
+//   5: 4,
+// };
+
+// const arrayLikeArray = Array.from(arrayLike);
+// const arrayLikeArray2 = Object.entries(arrayLike).map(([key, value]) => {
+//   if (typeof value === "string") {
+//     return [key, "changed"];
+//   }
+//   return [key, value];
+// });
+// console.log(arrayLikeArray2);
+// console.log(Object.fromEntries(arrayLikeArray2));
