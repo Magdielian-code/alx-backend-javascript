f86da1ac76c588f3f55fb33b0f111db87c35dd24
magdielian-code <oziomaagaecheta81@gmail.com>
2024-10-28 15:06:06 +0100

    task: Write a Class named StudentClass:
        The constructor accepts firstName(string) and lastName(string) arguments
        The class has a method named workOnHomework that return the string Currently working
        The class has a method named displayName. It returns the firstName of the student
        The constructor of the class should be described through an Interface
        The class should be described through an Interface
    
    Requirements:
    
        You can reuse the Webpack configuration from the previous exercise to compile the code.
        When running npm run build, no TypeScript error should be displayed.
        Every variable should use TypeScript when possible.

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,9 @@
+// Example usage
+var teacher3 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,63 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Index signature to allow additional properties
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const fullName = `${firstLetter}. ${lastName}`;
+  return fullName; 
+};
+
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const teacher3: Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: false,
+  location: "London",
+  contract: false,
+};
+
+console.log(teacher3);
