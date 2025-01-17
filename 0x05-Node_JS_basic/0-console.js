cee16b60187d61193b33d389216385c9bf33cca0
magdielian-code <oziomaagaecheta81@gmail.com>
2025-01-17 19:25:38 +0100

    fixed nodejs basic project

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,11 +1,5 @@
+function displayMessage(message) {
+  console.log(message);
+}
 
-/*
-In the file 0-console.js, create a function named
-displayMessage that prints in STDOUT the string argument.
-*/
-const displayMessage = (arg) => {
-    console.log(arg);
-  };
-  
-  module.exports = displayMessage;
-  
+module.exports = displayMessage;
